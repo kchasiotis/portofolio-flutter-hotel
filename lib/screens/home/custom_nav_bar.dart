cd79be3d0ab910e1fe34a904399517bce2096b93
kchasiotis <chasiotis.konstantinos.dev@gmail.com>
2024-05-04 23:02:22 +0300

    Adds home to navbar

--- lib/screens/home/custom_nav_bar.dart
+++ lib/screens/home/custom_nav_bar.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:portofolio/screens/getaways/widgets/input_widget.dart';
+
+Color bottonNavBgColor = Color(0xff0C1015);
 
 class CustomNavBar extends StatefulWidget {
   static int _selectedIndex = 0;
@@ -18,34 +22,56 @@ class CustomNavBarState extends State<CustomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-
-    return BottomNavigationBar(
-        elevation: 0, // to get rid of the shadow
-        currentIndex: CustomNavBar._selectedIndex,
-        selectedItemColor: Colors.white,
-        onTap: _onItemTapped,
-        backgroundColor: Color(0xff0C1015).withOpacity(0.99), // transparent, you could use 0x44aaaaff to make it slightly less transparent with a blue hue.
-        type: BottomNavigationBarType.fixed,
-        unselectedItemColor: Colors.grey,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'home',
+    return SafeArea(
+        child: Container(
+      height: 90,
+      //TODO: In Future remove the height
+      padding: const EdgeInsets.all(12),
+      margin: const EdgeInsets.symmetric(horizontal: 0),
+      decoration: BoxDecoration(
+        color: bottonNavBgColor.withOpacity(0.9),
+        border: Border(top: BorderSide(color: Colors.white.withOpacity(0.2))),
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(16), topRight: Radius.circular(16)),
+        boxShadow: [
+          BoxShadow(
+            color: bottonNavBgColor.withOpacity(0.3),
+            offset: const Offset(0, 20),
+            blurRadius: 20,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.message_rounded),
-            label: 'inbox',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'favourites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'profile',
-          ),
-        ]
-    );
+        ],
+      ),
+      // TODO: Animated Icons,
+      child: Padding(
+          padding:
+              const EdgeInsets.only(left: 50, right: 50, top: 10, bottom: 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  SvgPicture.asset(
+                    'images/nav_bar/home-cropped.svg',
+                    width: 30,
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 3),
+                    child: Text(
+                      'home',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )),
+    ));
   }
 
   @override
