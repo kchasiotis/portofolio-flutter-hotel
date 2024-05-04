8de5344d960414c4d7221e12edea26a0f869a0b7
kchasiotis <chasiotis.konstantinos.dev@gmail.com>
2024-05-04 17:17:14 +0300

    Make view scrollable

--- lib/screens/getaways/find-getaways.dart
+++ lib/screens/getaways/find-getaways.dart
@@ -8,108 +8,112 @@ class FindGetaways extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: const Color(0xff0C1015),
-        child: Column(children: <Widget>[
-          const Padding(
-              padding: EdgeInsets.only(left: 30, right: 30, top: 70),
-              child: Text(
-                'Find the best getaways for you',
-                style: TextStyle(fontSize: 32, color: Colors.white),
-              )),
-          Container(
-            height: 24,
-          ),
-          Container(
-            height: 24,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 30, right: 30),
-            child: InputWidget(),
-          ),
-          Container(
-            height: 24,
-          ),
-          Padding(
-              padding: const EdgeInsets.only(
-                left: 30,
+    return SingleChildScrollView(
+        child: Container(
+            color: const Color(0xff0C1015),
+            child: Column(children: <Widget>[
+              const Padding(
+                  padding: EdgeInsets.only(left: 30, right: 30, top: 70),
+                  child: Text(
+                    'Find the best getaways for you',
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  )),
+              Container(
+                height: 24,
               ),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: <Widget>[
-                    const Text(
-                      'Luxe',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                        decorationColor: Colors.white,
+              Container(
+                height: 24,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 30, right: 30),
+                child: InputWidget(),
+              ),
+              Container(
+                height: 24,
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                  ),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: <Widget>[
+                        const Text(
+                          'Luxe',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white,
+                          ),
+                        ),
+                        Container(
+                          width: 16,
+                        ),
+                        const Text(
+                          'Countryside',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff39414E),
+                          ),
+                        ),
+                        Container(
+                          width: 16,
+                        ),
+                        const Text(
+                          'Islands',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff39414E),
+                          ),
+                        ),
+                        Container(
+                          width: 16,
+                        ),
+                        const Text(
+                          'Private rooms',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff39414E),
+                          ),
+                        ),
+                        Container(
+                          width: 16,
+                        ),
+                        const Text(
+                          'Desert',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff39414E),
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+              Container(
+                height: 20,
+              ),
+              Container(
+                child: Padding(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [HotelCard(), HotelCard()],
                       ),
-                    ),
-                    Container(
-                      width: 16,
-                    ),
-                    const Text(
-                      'Countryside',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xff39414E),
+                      Container(
+                        height: 20,
                       ),
-                    ),
-                    Container(
-                      width: 16,
-                    ),
-                    const Text(
-                      'Islands',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xff39414E),
-                      ),
-                    ),
-                    Container(
-                      width: 16,
-                    ),
-                    const Text(
-                      'Private rooms',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xff39414E),
-                      ),
-                    ),
-                    Container(
-                      width: 16,
-                    ),
-                    const Text(
-                      'Desert',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xff39414E),
-                      ),
-                    )
-                  ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [HotelCard(), HotelCard()],
+                      )
+                    ],
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 30),
                 ),
-              )),
-          Container(
-            height: 20,
-          ),
-          Container(
-            child: Padding(child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [HotelCard(), HotelCard()],
-                ),
-                Container(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [HotelCard(), HotelCard()],
-                )
-
-              ],
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 30),),
-          )
-        ]));
+              )
+            ])));
   }
 }
