c7fa0ac6427203f4502f611249b4951300dda20e
kchasiotis <chasiotis.konstantinos.dev@gmail.com>
2024-05-05 19:22:19 +0300

    Feature: Getaway screen top half

--- lib/screens/getaway_stay/getaway_stay.dart
+++ lib/screens/getaway_stay/getaway_stay.dart
@@ -20,20 +20,36 @@ class GetawayStay extends StatelessWidget {
                 height: 60,
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
-                  // color: Colors.red.withOpacity(0.8),
                   color: const Color(0xff0C1015).withOpacity(0.95),
                   border: Border(
                       top: BorderSide(
                           color: Colors.white.withOpacity(0.2), width: 1)),
-                  borderRadius: BorderRadius.only(
+                  borderRadius: const BorderRadius.only(
                       topRight: Radius.circular(32),
                       topLeft: Radius.circular(32)),
                 ),
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 30, right: 30),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Urban Luxe',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 26)),
+                          Text(
+                            '3,899\$',
+                            style: TextStyle(color: Colors.white, fontSize: 24),
+                          )
+                        ]),
+                  ),
+                ),
               )),
-              Positioned(
-                  bottom: 43,
-                  right: 30,
-                  child: Container(
+          Positioned(
+              bottom: 43,
+              right: 30,
+              child: Container(
                   height: 33,
                   width: 74,
                   decoration: BoxDecoration(
@@ -48,7 +64,11 @@ class GetawayStay extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Image.asset('images/getaway/start.png', width: 16,height: 16,),
+                      Image.asset(
+                        'images/getaway/start.png',
+                        width: 16,
+                        height: 16,
+                      ),
                       Container(width: 2),
                       const Text(
                         '4.8',
